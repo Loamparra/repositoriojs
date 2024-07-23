d9e420e677c608c63864affb9bc3106e10d6b3a4
Loana Parra <loa.parra01@gmail.com>
2024-07-22 22:14:45 -0300

    commit 2

--- js/app.js
+++ js/app.js
@@ -1,28 +1,66 @@
-function registro(){
-    let nombre = prompt('Ingrese su nombre: ');
-    alert('Hola ' + nombre + ', te doy la bienvenida a nuestra web');
-    let opcion = prompt( nombre + ', te invitamos a llenar un formulario para recibir novedades u ofertas de nuestra página web \nIngresa "si" o "no" para continuar.');
-    if ((opcion == 'si') || (opcion == 'SI') || (opcion == 'Si')) {
-        let apellido = prompt('Ingrese su apellido: ');
-        let pais = prompt('Ingrese el país: ');
-        let email = prompt('Ingrese su gmail: ');
-        let edad = parseInt(prompt('Ingrese su edad: '));
-        if (isNaN(edad)){
-            alert('No ingresaste números. Volvé a intentarlo.');
-        } else{
-            alert('¡Gracias ' + nombre + ' ' + apellido + ', te enviaremos por gmail nuestra informacion!\nLos datos ingresados son: \nNombre: '+ nombre +'\nApellido: '+ apellido +'\nEdad: '+ edad +'\nPaís: '+ pais +'\nGmail: '+ email + '\nSi los datos están mal ingresados, intente nuevamente.');
-        }
-    
-    } else if ((opcion == 'no') || (opcion == 'NO') || (opcion == 'No')){
-        alert('¡Adiós ' + nombre + '!');
+let sistema_stock = new Sistema(productos)
 
-    } else {
-        alert('No ingresaste ninguna de las opciones correspondientes.');
-    } 
-    
+let menu_str = `
+Elegí la opción que quieras realizar.
+1 - Aumentar todos los precios.
+2 - Agregar producto nuevo.
+3 - Modificar un producto.
+4 - Ver lista de productos.
+0 - Cancelar operación.
+`
+
+function ingresarDatos(tipo, texto, otro = '') {
+  let datos;
+
+  if (tipo == 'numero') {
+    do {
+      datos = parseFloat(prompt(texto, otro));
+    } while (isNaN(datos) || datos < 0);
+  } else if (tipo == 'nombre') {
+    do {
+      datos = prompt(texto, otro).toLocaleLowerCase();
+    } while (datos == '');
+  }
+  return datos;
 }
-registro();
 
+let clave = prompt('Ingrese la contraseña.');
+while (clave != '2024FAIV') {
+  switch (clave) {
+    case '2024FAIV':
+      alert('Bienvenido');
+      break;
+    default:
+      alert('Contraseña incorrecta, volvé a intentarlo.');
+      break;
+  }
+  clave = prompt('Ingrese la contraseña.');
+}
 
-
-
+let cancelar = false;
+while (!cancelar) {
+  let opcion = parseInt(prompt(menu_str));
+  switch (opcion) {
+    case 0:
+      alert('Cancelaste la operacion.')
+      cancelar = true;
+      break;
+    case 1:
+      sistema_stock.aumentoPrecios();
+      break;
+    case 2:
+      sistema_stock.nuevoProducto();
+      break;
+    case 3:
+      sistema_stock.editarProducto();
+      break;
+    case 4:
+      sistema_stock.mostrarTabla();
+      break;
+    case 5:
+      sistema_stock.filtrarProducto();
+      break;
+    default:
+      alert('No existe esta opción. Volvé a intentartlo.')
+  }
+}
